2078d0b05f115445c85aea1335920872fbb24267
Rpergy <55565680+Rpergy@users.noreply.github.com>
2021-03-10 23:08:16 -0500

    Changed inputs to match values

--- source.js
+++ source.js
@@ -5,12 +5,24 @@ $(document).on('change','#planSelection',function(){
         updatePrice();
     }
     else if(plan == "basic") {
+        $("#standardCount").val("3");
+        $("#proCount").val("0");
+        $("#businessCount").val("0");
+        
         $("#price").html("$90");
     }
     else if(plan == "professional") {
+        $("#standardCount").val("0");
+        $("#proCount").val("5");
+        $("#businessCount").val("0");
+
         $("#price").html("$300");
     }
     else if(plan == "business") {
+        $("#standardCount").val("0");
+        $("#proCount").val("0");
+        $("#businessCount").val("10");
+        
         $("#price").html("$900");
     }
 });
