4e2495cf5dca6713c90a7c58b0209d1636112efa
Rpergy <rpergola0729@gmail.com>
2021-03-13 21:57:33 -0500

    added a submit button

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## Rpergy's main website repo
 
 Things I need to do:
-- "About Me" page
 - Finish checkout page (Paypal dev acc) (Currently Doing)
 - Footer
--- source.js
+++ source.js
@@ -1,3 +1,33 @@
+var plan = $("#planSelection").val();
+
+if(plan == "custom"){
+    $("#standardCount").val("0");
+    $("#proCount").val("0");
+    $("#businessCount").val("0");
+    updatePrice();
+}
+else if(plan == "basic") {
+    $("#standardCount").val("3");
+    $("#proCount").val("0");
+    $("#businessCount").val("0");
+        
+    $("#price").html("$90");
+}
+else if(plan == "professional") {
+    $("#standardCount").val("0");
+    $("#proCount").val("5");
+    $("#businessCount").val("0");
+
+    $("#price").html("$300");
+}
+else if(plan == "business") {
+    $("#standardCount").val("0");
+    $("#proCount").val("0");
+    $("#businessCount").val("10");
+        
+    $("#price").html("$900");
+}
+
 $(document).on('change','#planSelection',function(){
     var plan = $("#planSelection").val();
 
@@ -38,8 +68,23 @@ function updatePrice() {
 
     var fullPrice = standardPrice + proPrice + businessPrice;
 
-    console.log(fullPrice);
-
     $("#price").html("$" + fullPrice);
 }
 $("input").change(updatePrice);
+
+function submit(event){
+    event.preventDefault();
+
+    var data = {
+        standardPages: parseInt($("#standardCount").val()),
+        proPages: parseInt($("#proCount").val()),
+        businessPages: parseInt($("#businessCount").val()),
+
+        name: $("#nameInput").val(),
+        email: $("#emailInput").val(),
+        phone: $("#phoneInput").val()
+    };
+
+    console.log(data);
+}
+$("#submit").click(submit);
